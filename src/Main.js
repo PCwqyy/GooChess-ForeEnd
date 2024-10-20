5b8e62e78065d94e6258bcf95a657d134ef2235d
PCwqyy <w123567910@outlook.com>
2024-10-20 11:25:53 +0800

    完善棋盘 添加游戏三方、发射器

--- src/Main.js
+++ src/Main.js
@@ -2,7 +2,6 @@ const Grood=document.getElementById('Grood');
 const rows=12;
 const RomeApl={1:'i',2:'ii',3:'iii',4:'iv',5:'v',6:'vi',7:'vii',8:'viii',9:'ix',10:'x',11:'xi',12:'xii',13:'xiii',14:'xiv'};
 
-
 class XY{
 	constructor(x,y){
 		this.x=x;
@@ -19,7 +18,6 @@ class NAR{
 		return `${this.num},${String.fromCharCode(96+this.alp)},${RomeApl[this.rom]}`;
 	}
 }
-
 function XYtoNAR(a){
 	return new NAR(
 		Math.floor(a.x/2)+1,
@@ -34,6 +32,14 @@ function NARtoXY(a){
 
 var row=new Array;
 var cell=new Array;
+const Shooter={
+	1:new XY(15,4),2:new XY(15,5),3:new XY(17,5),
+	4:new XY(7,1),5:new XY(9,1),6:new XY(7,4),
+	7:new XY(9,5),8:new XY(15,1),9:new XY(17,1),
+	10:new XY(23,4),11:new XY(23,5),12:new XY(23,8),
+	13:new XY(23,9),14:new XY(17,9),15:new XY(15,8)
+};
+
 
 function CreateBoard(){
 	for(let i=1;i<=rows*2-1;i++){
@@ -41,16 +47,19 @@ function CreateBoard(){
 		row[i].classList.add('row');
 		cell[i]=new Array;
 		if(i%2==0)
-			row[i].classList.add('odd');
+			row[i].classList.add('black');
 		for(let j=1;j<=Math.floor((i-1)/2)+1;j++){
 			cell[i][j]=document.createElement('div');
 			cell[i][j].classList.add('cell');
-			cell[i][j].style.transform="rotate(180deg)";
-			if(i%2==1){
-				cell[i][j].style.transform="rotate(0deg)";
-				cell[i][j].classList.add('light');
-			}
-			if(i)
+			for(q in Shooter)
+				if(Shooter[q].x==i&&Shooter[q].y==j){
+					cell[i][j].classList.add('shooter');
+					break;
+				}
+			var thisNar=XYtoNAR(new XY(i,j));
+			if(thisNar.num<=4)	cell[i][j].classList.add('red');
+			if(thisNar.alp<=4)	cell[i][j].classList.add('blue');
+			if(thisNar.rom<=4)	cell[i][j].classList.add('yellow');
 			row[i].appendChild(cell[i][j]);
 			cell[i][j].addEventListener('click',()=>{
 				console.log(`(${i},${j}) (${XYtoNAR(new XY(i,j)).print()})`);
