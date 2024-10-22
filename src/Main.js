0c1ce5e5e32db63d6df1ff72a271ae5924b66128
PCwqyy <w123567910@outlook.com>
2024-10-22 22:33:01 +0800

    修复棋子在Scaled中的移动

--- src/Main.js
+++ src/Main.js
@@ -1,5 +1,5 @@
-const Grood=document.getElementById('Grood');
-var choos=document.getElementById('testchoos');
+const body=document.getElementsByTagName('body')[0];
+var Goord;
 const rows=12;
 const RomeApl={1:'i',2:'ii',3:'iii',4:'iv',5:'v',6:'vi',7:'vii',8:'viii',9:'ix',10:'x',11:'xi',12:'xii',13:'xiii',14:'xiv'};
 
@@ -25,11 +25,11 @@ function XYtoNAR(a){
 		Math.floor(a.x/2)+1,
 		Math.floor((2*rows+2*a.y-a.x)/2),
 		rows-a.y+1);
-}
-function NARtoXY(a){
-	b=new XY(2*a.num-2,rows-a.rom+1);
-	if(2*rows+2*b.y-b.x!=2*a.alp)	b.x++;
-	return b;
+	}
+	function NARtoXY(a){
+		b=new XY(2*a.num-2,rows-a.rom+1);
+		if(2*rows+2*b.y-b.x!=2*a.alp)	b.x++;
+		return b;
 }
 
 // 布置棋盘
@@ -44,22 +44,24 @@ var row=new Array;
 /** @type {Array< Array<Element> >} */
 var cell=new Array;
 
-function CreateBoard(){
+function CreateBoard(id){
+	var thisGrood=document.createElement('grood');
+	thisGrood.id=id;
+	body.appendChild(thisGrood);
 	for(let i=1;i<=rows*2-1;i++){
 		row[i]=document.createElement('div');
 		row[i].classList.add('row');
 		cell[i]=new Array;
 		if(i%2==0)	row[i].classList.add('black');
 		for(let j=1;j<=Math.floor((i-1)/2)+1;j++){
-			cell[i][j]=document.createElement('div');
-			cell[i][j].classList.add('cell');
+			cell[i][j]=document.createElement('cell');
 			// 确定shooter
 			for(q in Shooter)
 				if(Shooter[q].x==i&&Shooter[q].y==j){
 					cell[i][j].classList.add('shooter');
 					break;
 				}
-			// 确定三方位置
+				// 确定三方位置
 			var thisNar=XYtoNAR(new XY(i,j));
 			if(thisNar.num<=4)	cell[i][j].classList.add(Side.num);
 			if(thisNar.alp<=4)	cell[i][j].classList.add(Side.alp);
@@ -71,8 +73,9 @@ function CreateBoard(){
 			});
 			row[i].appendChild(cell[i][j]);
 		}
-		Grood.appendChild(row[i]);
+		thisGrood.appendChild(row[i]);
 	}
+	return thisGrood;
 }
 
 function QueryPos(x,y){
@@ -82,9 +85,9 @@ function QueryPos(x,y){
 	if(x%2==1)	return {left:pos.x+pos.width/2,top:pos.y+pos.height*2/3};
 	else		return {left:pos.x+pos.width/2,top:pos.y+pos.height/3};
 }
-/** @param {Element} ch */
+/** @param {Element} ch choos */
 function ChoosToCell(ch,x,y){
-	var posh=ch.getBoundingClientRect();
+	var posh=MeasurerChoos.getBoundingClientRect();
 	ch.style.left=`${QueryPos(x,y).left-posh.width/2}px`;
 	ch.style.top=`${QueryPos(x,y).top-posh.height/2}px`;
 }
@@ -92,3 +95,12 @@ function ChoosToCell(ch,x,y){
 function ScaleBoard(){
 	Grood.classList.toggle("Scaled");
 }
+
+
+
+
+
+var choos=document.createElement('choos');
+choos.id='testchoos';
+var MeasurerChoos=document.createElement('choos');
+MeasurerChoos.id='measurerChoos';
