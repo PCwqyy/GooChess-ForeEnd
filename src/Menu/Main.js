33cd7fa4b42be1b588f00926b48c940d3b32ba52
PCwqyy <w123567910@outlook.com>
2024-10-24 23:17:07 +0800

    增加主界面三角形跳动

--- src/Menu/Main.js
+++ src/Menu/Main.js
@@ -1,6 +1,6 @@
 var MainTri=document.getElementById('MainTriangle');
 var now=0;
-setInterval(()=>{
-	now+=Math.random()*120-60;
-	MainTri.style.transform=`rotate(${now}deg)`;
-},300)
+// setInterval(()=>{
+// 	now+=Math.random()*120-60;
+// 	MainTri.style.transform=`rotate(${now}deg)`;
+// },300)
