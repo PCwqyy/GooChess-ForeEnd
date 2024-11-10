a545ec3ff8d62ae8a80075177681f6bee1fa1c3f
PCwqyy <w123567910@outlook.com>
2024-11-10 17:39:42 +0800

    Edit...

--- src/Menu/Main.js
+++ src/Menu/Main.js
@@ -1,6 +1,21 @@
-var MainTri=document.getElementById('MainTriangle');
-var now=0;
-// setInterval(()=>{
-// 	now+=Math.random()*120-60;
-// 	MainTri.style.transform=`rotate(${now}deg)`;
-// },300)
+var Body=document.getElementById('Home')
+setInterval(()=>{
+	var t=document.createElement('span');
+	t.classList.add('SmallT');
+	t.style.top=`${Math.random()*100}vh`;
+	var size=Math.random()*6+5;
+	console.log(size);
+	t.style.width=`${size*1}vh`;
+	t.style.height=`${size/2*Math.sqrt(3)}vh`;
+	t.style.backgroundColor=`hsl(${Math.random()*360} 80% 50% / 0.8)`;
+	var speed=Math.random()*5+3;
+	Body.appendChild(t);
+	t.style.transition=`${speed}s`;
+	setTimeout(() => {
+		t.style.left='110vw';
+		t.style.rotate='1800deg'
+	},10);
+	setTimeout(() => {
+		Body.removeChild(t);
+	},speed*1000+200);
+},300)
