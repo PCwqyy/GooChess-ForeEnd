a4467fb39db8d144ea4e7aa47ab8a20ee260ad32
PCwqyy <w123567910@outlook.com>
2024-11-17 00:28:20 +0800

    大 更 新 完成了主界面菜单

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 好棋！真是一盘好棋啊！
 
 PClang Ver: Dev 0.1
-Ver: 0.1
-2024/11/13 PCwqyy
+Ver: 0.2
+2024/11/17 PCwqyy
---
+++ src/Menu/Welcome.js
@@ -0,0 +1,36 @@
+var Body=document.getElementById('Home');
+var BigT=document.getElementById('MainTriangle');
+var StSh=document.getElementById('MenuSt');
+var WelTxt=document.getElementsByClassName('welcome');
+
+var thr=setInterval(()=>{
+	var t=document.createElement('span');
+	t.classList.add('SmallT');
+	t.style.top=`${Math.random()*100}vh`;
+	var size=Math.random()*6+5;
+	t.style.width=`${size*1}vh`;
+	t.style.height=`${size/2*Math.sqrt(3)}vh`;
+	t.style.backgroundColor=`hsl(${Math.random()*360} 80% 50%`;
+	var speed=Math.random()*5+5;
+	Body.appendChild(t);
+	t.style.transition=`${speed}s`;
+	setTimeout(() => {
+		t.style.left='110vw';
+		t.style.rotate='1800deg'
+	},20);
+	setTimeout(() => {
+		Body.removeChild(t);
+	},speed*1000);
+},200);
+var Zoomed=false;
+document.addEventListener('keydown',(event)=>{
+	if(Zoomed)	return;
+	Zoomed=true;
+	clearInterval(thr);
+	BigT.style.animation
+	StSh.setAttribute('href','Menu.css');
+	setTimeout(()=>{
+		for(var i=0;i<WelTxt.length;i++)
+			WelTxt[i].style.display='none';
+	},2000);
+})
